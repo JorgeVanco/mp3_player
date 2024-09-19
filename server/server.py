53a962c8f5a2c65fbd5b647b8de9197404aa0e28
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2024-09-19 11:49:48 +0200

    added error message

--- server/server.py
+++ server/server.py
@@ -20,6 +20,20 @@ origins = [
 ]
 
 
+try:
+    myclient = pymongo.MongoClient(
+        f"mongodb+srv://{os.getenv('MONGODB_ATLAS_USERNAME')}:{os.getenv('MONGODB_ATLAS_PASSWORD')}@mondongo.edq7zvv.mongodb.net/?retryWrites=true&w=majority&appName=Mondongo"
+    )
+    mydb = myclient["mp3_player_db"]
+    song_order_collection = mydb["song_order"]
+    myclient.server_info()  # force connection on a request as the
+    # connect=True parameter of MongoClient seems
+    # to be useless here
+except pymongo.errors.ServerSelectionTimeoutError as err:
+    # do whatever you need
+    print("ERR", err)
+
+
 class Song(BaseModel):
     song_name: str
     author: str
@@ -35,18 +49,7 @@ async def start_scheduler(app: FastAPI):
 
 
 app = FastAPI(lifespan=start_scheduler)
-try:
-    myclient = pymongo.MongoClient(
-        f"mongodb+srv://{os.getenv('MONGODB_ATLAS_USERNAME')}:{os.getenv('MONGODB_ATLAS_PASSWORD')}@mondongo.edq7zvv.mongodb.net/?retryWrites=true&w=majority&appName=Mondongo"
-    )
-    mydb = myclient["mp3_player_db"]
-    song_order_collection = mydb["song_order"]
-    myclient.server_info()  # force connection on a request as the
-    # connect=True parameter of MongoClient seems
-    # to be useless here
-except pymongo.errors.ServerSelectionTimeoutError as err:
-    # do whatever you need
-    print(err)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -91,4 +94,5 @@ async def add_song_reproduction(song: Song):
 
 
 if __name__ == "__main__":
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
