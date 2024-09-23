2962155fe49ced121433ae8e79705876d2ba0306
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2024-09-23 18:48:52 +0200

    added stopPropagation to login in song card

--- src/components/LogInButton.js
+++ src/components/LogInButton.js
@@ -2,6 +2,6 @@ import { FcGoogle } from "react-icons/fc";
 import {signInWithGoogle} from '../functions/auth';
 
 const LogInButton = ({setUser}) => {
-    return <button className="blue-btn" onClick={() => signInWithGoogle(setUser)}><FcGoogle style={{position: "relative", top:"2px"}}/> Log In</button>
+    return <button className="blue-btn" onClick={(e) => {e.stopPropagation();signInWithGoogle(setUser)}}><FcGoogle style={{position: "relative", top:"2px"}}/> Log In</button>
 }
 export default LogInButton;
